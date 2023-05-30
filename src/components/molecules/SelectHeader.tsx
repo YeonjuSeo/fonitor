b08907266230cf9b96618f7627ed5f58aacadadd
YeonjuSeo <tjduswn1219@gmail.com>
2023-05-30 16:42:11 +0900

    ✨ UPDATE: send endtime to server

--- src/components/molecules/SelectHeader.tsx
+++ src/components/molecules/SelectHeader.tsx
@@ -1,5 +1,8 @@
 import DecisionBtn, { ButtonType } from "@components/atoms/DecisionBtn";
+import { customerInfoState } from "@recoil/customer";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 export const SelectHeader = ({
   title,
@@ -7,6 +10,23 @@ export const SelectHeader = ({
   okTxt,
 }: SelectHeaderProps) => {
   const navigate = useNavigate();
+  const customerInfo = useRecoilValue(customerInfoState);
+
+  const sendProcessEndTime = () => {
+    let timezoneOffset = new Date().getTimezoneOffset() * 60000;
+    let timezoneDate = new Date(Date.now() - timezoneOffset);
+
+    axios
+      .put(
+        `${process.env.REACT_APP_BE_API_END_POINT}/customer/${customerInfo.customerId}`,
+        {
+          endTime: timezoneDate.toISOString(),
+        }
+      )
+      .then((resp) => console.log(resp.statusText))
+      .catch((err) => console.log(err));
+  };
+  const sendVideo = () => {};
 
   return (
     <div className="mb-4 bg-gradient-to-r from-purple-600 to-indigo-600 grid grid-cols-3 grid-flow-row gap-4 place-items-center border-white border-2 rounded-lg p-1 w-full border-opaicty-100">
@@ -27,15 +47,17 @@ export const SelectHeader = ({
         </div>
       )}
       {okTxt !== "" && (
-        // <div className="col-start-3 col-span-3 w-full">
         <DecisionBtn
           type={ButtonType.Ok}
           text={okTxt}
-          onClick={() =>
-            navigate("/processing", { state: { type: "거래처리" } })
-          }
+          onClick={() => {
+            if (title === "비밀번호") {
+              sendProcessEndTime();
+              sendVideo();
+              navigate("/processing", { state: { type: "거래처리" } });
+            }
+          }}
         />
-        // </div>
       )}
     </div>
   );
