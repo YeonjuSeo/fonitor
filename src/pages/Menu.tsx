86416d442eed1b0c0d638f8ccca8db2f9b233de8
YeonjuSeo <tjduswn1219@gmail.com>
2023-05-30 16:30:45 +0900

    ✨ UPDATE: send debit and record video to judge

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,28 +1,49 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
 import {
   SelectHeader,
   SelectHeaderProps,
 } from "@components/molecules/SelectHeader";
 import { ATMLayout } from "@components/templates/ATMLayout";
 import { useLocation } from "react-router";
-import DecisionBtn, { ButtonType } from "../components/atoms/DecisionBtn";
 import { useNavigate } from "react-router-dom";
 import MenuBtn from "@components/atoms/MenuBtn";
 import InstructionContainer from "@components/molecules/InstructionContainer";
+import { useRecoilValue } from "recoil";
+import { customerInfoState } from "@recoil/customer";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Menu = () => {
+  const customerInfo = useRecoilValue(customerInfoState);
+
   const navigate = useNavigate();
   const location = useLocation();
   const state = location.state as { type: string };
   const type = state.type;
 
-  const handleNavigate: MouseEventHandler<HTMLButtonElement> = () => {
+  const handleNavigate = () => {
     if (type === "매체선택") {
       navigate("/menu", { state: { type: "금액선택" } });
     } else if (type === "금액선택") {
       navigate("/pw");
     }
   };
+
+  const sendDebit = (debitMoney: number) =>
+    axios
+      .post(`${process.env.REACT_APP_BE_API_END_POINT}/result`, {
+        customerId: customerInfo.customerId,
+        withdraw: debitMoney,
+      })
+      .then((resp) => {
+        console.log(resp.statusText);
+        handleNavigate();
+      })
+      .catch((err) => console.log(err));
+
+  useEffect(() => {
+    document.getElementById("record_btn")!.click();
+  }, []);
 
   return (
     <ATMLayout>
@@ -33,11 +54,25 @@ const Menu = () => {
       />
       <main className="grid grid-cols-4 grid-flow-row gap-5 place-items-center">
         {type === "매체선택" &&
-          MediumList.map((e) => <MenuBtn text={e} onClick={handleNavigate} />)}
+          MediumList.map((e, i) => (
+            <MenuBtn
+              key={e + i}
+              disabled={e === "카드" ? false : true}
+              text={e}
+              onClick={handleNavigate}
+            />
+          ))}
         {type === "금액선택" && (
           <React.Fragment>
-            {PricesList.map((e) => (
-              <MenuBtn text={e + "만원"} onClick={handleNavigate} />
+            {PricesList.map((price) => (
+              <MenuBtn
+                key={price + "만원"}
+                text={price + "만원"}
+                onClick={(e) => {
+                  document.getElementById("record_btn")!.click();
+                  sendDebit(price);
+                }}
+              />
             ))}
             <button className="w-full bg-orange-400 text-2xl font-medium p-2 rounded-lg shadow-xl">
               기타
@@ -48,14 +83,6 @@ const Menu = () => {
           <InstructionContainer type={type} />
         </div>
       </main>
-
-      {type === "카드" && (
-        <DecisionBtn
-          type={ButtonType.Normal}
-          text="카드"
-          onClick={handleNavigate}
-        />
-      )}
     </ATMLayout>
   );
 };
@@ -80,3 +107,4 @@ const MediumList = [
 ];
 
 const PricesList = [3, 5, 10, 15, 20, 30, 40, 50, 60, 70, 100];
+const pwBoardList = [1, "*", 2, 3, 4, 5, 6, 7, "*", 8, 9, 0];
