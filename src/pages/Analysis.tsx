ee9f17d0c2f7dfdf2ca36a29d00af95f4d88c8fd
YeonjuSeo <tjduswn1219@gmail.com>
2023-05-30 16:40:30 +0900

    ✨ UPDATE: get video data from server

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -1,29 +1,19 @@
-import { useRef } from "react";
-import video2 from "@assets/video.mp4";
+import { useRef, useState, useEffect } from "react";
 import useVideoPlayer from "@hooks/useVideoPlayer";
 import { MdPause, MdPlayArrow } from "react-icons/md";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import { VideoMockData } from "@recoil/video";
+import VideoListItemBtn from "@components/atoms/VideoListItemBtn";
+import axios from "axios";
+import { userInfoState } from "@recoil/user";
+import { useRecoilValue } from "recoil";
+import EmotionRatioChart from "@components/molecules/EmotionRatioChart";
+import ValidEmotionRatioChart from "@components/molecules/ValidEmotionRatioChart";
 
 const Analysis = () => {
   const videoElement = useRef(null);
+  const [videoList, setVideoList] = useState(VideoMockData);
+  const [whichIsClicked, setWhichIsClicked] = useState(0);
+  const userInfo = useRecoilValue(userInfoState);
   const {
     playerState,
     togglePlay,
@@ -32,221 +22,101 @@ const Analysis = () => {
     handleVideoSpeed,
   } = useVideoPlayer(videoElement);
 
-  const returnNewChart = () => {
-    return <Bar options={options} data={data} />;
+  const getVideoData = () => {
+    const params = { username: userInfo.username };
+    axios
+      .get(
+        `${process.env.REACT_APP_BE_API_END_POINT}/users/${userInfo.username}/lists`,
+        {
+          params,
+        }
+      )
+      .then((resp) => {
+        setVideoList(resp.data);
+      })
+      .catch((err) => console.log(err));
   };
-  return (
-    <div className="p-2 flex flex-col items-center h-screen bg-gray-900">
-      <div className="flex text-white font-semibold justify-around w-full mt-6 mb-3">
-        <div>신한 은행 / 이화여자대학교 지점</div>
-        <div>No.e021fz</div>
-        <div>2022.11.21 17:56:02</div>
-      </div>
-      <main className="justify-between items-between">
-        {/* <main className="justify-between items-between grid grid-cols-2 grid-flow-row gap-4 place-items-center"> */}
-        <div className="container">
-          <div className="video-wrapper">
-            <video
-              src={video2}
-              ref={videoElement}
-              onTimeUpdate={handleOnTimeUpdate}
-            />
-            <div className="controls flex items-center px-3">
-              <div className="actions flex items-center">
-                <button onClick={togglePlay}>
-                  {!playerState.isPlaying ? (
-                    <MdPlayArrow className="text-4xl text-white" />
-                  ) : (
-                    <MdPause className="text-4xl text-white" />
-                  )}
-                </button>
-              </div>
 
-              <input
-                className="mx-2 form-range bg-transparent w-full h-2 p-0 rounded-lg appearance-none cursor-pointer dark:bg-gray-200"
-                type="range"
-                min="0"
-                max="100"
-                value={playerState.progress}
-                onChange={(e) => handleVideoProgress(e)}
-              />
-              {/* <select
-                className="velocity"
-                value={playerState.speed}
-                onChange={(e) => handleVideoSpeed(e)}
-              >
-                <option value="0.50">0.50x</option>
-                <option value="1">1x</option>
-                <option value="1.25">1.25x</option>
-                <option value="2">2x</option>
-              </select> */}
+  useEffect(() => {
+    getVideoData();
+  }, []);
+
+  return (
+    <div className="flex bg-gray-900 w-full h-full">
+      <div className="bg-gray-700 w-72 flex justify-center py-5 px-2 overflow-scroll h-screen ">
+        <div className="flex flex-col items-end ">
+          {videoList.map((video, i) => (
+            <VideoListItemBtn
+              isVictim={video.victim}
+              isClicked={whichIsClicked === i ? true : false}
+              startTime={video.startTime}
+              onClick={() => {
+                setWhichIsClicked(i);
+              }}
+            />
+          ))}
+        </div>
+      </div>
+      {videoList.map(
+        (video, i) =>
+          whichIsClicked === i && (
+            <div className="p-2 w-10/12 flex flex-col items-center h-screen overflow-scroll">
+              <div className="flex text-white font-semibold justify-around w-full mt-6 mb-3">
+                <div>신한 은행 / 이화여자대학교 지점</div>
+                <div>{video.atmId}</div>
+                <div>
+                  {video.startTime}/{video.endTime}
+                </div>
+              </div>
+              <main className="flex flex-col justify-between items-center w-full px-10 py-10 ">
+                <div className="container px-10 flex justify-center ">
+                  <div className="video-wrapper flex flex-col w-2/3">
+                    <video
+                      src={video.filePath}
+                      ref={videoElement}
+                      onTimeUpdate={handleOnTimeUpdate}
+                    />
+                    <div className="controls flex items-center px-3">
+                      <div className="actions flex items-center">
+                        <button onClick={togglePlay}>
+                          {!playerState.isPlaying ? (
+                            <MdPlayArrow className="text-4xl text-white" />
+                          ) : (
+                            <MdPause className="text-4xl text-white" />
+                          )}
+                        </button>
+                      </div>
+                      <input
+                        className="mx-2 form-range bg-transparent w-full h-2 p-0 rounded-lg appearance-none cursor-pointer dark:bg-gray-200"
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={playerState.progress}
+                        onChange={(e) => handleVideoProgress(e)}
+                      />
+                      <select
+                        className="velocity"
+                        value={playerState.speed}
+                        onChange={(e) => handleVideoSpeed(e)}
+                      >
+                        <option value="0.50">0.50x</option>
+                        <option value="1">1x</option>
+                        <option value="1.25">1.25x</option>
+                        <option value="2">2x</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-4 py-5 w-3/4 ">
+                  <EmotionRatioChart data={video.emotions} />
+                  <ValidEmotionRatioChart data={video.emotions} />
+                </div>
+              </main>
             </div>
-          </div>
-        </div>
-        <div className="h-1/6 w-full">
-          <Bar options={options} data={data} />
-          {/* <Bar options={options} data={data2} height={300} /> */}
-        </div>
-        <div className="h-1/6 w-full">
-          {/* <Bar options={options} data={data3} /> */}
-          {/* <Bar options={options} data={data2} height={300} /> */}
-        </div>
-      </main>
+          )
+      )}
     </div>
   );
 };
 
 export default Analysis;
-
-const labels = ["유효 감정 빈도"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "유효 감정",
-      data: [578],
-      backgroundColor: "#3C39C0",
-    },
-    {
-      label: "그외",
-      data: [341],
-      backgroundColor: "rgb(75, 192, 192)",
-    },
-  ],
-};
-
-const labels2 = ["감정 빈도"];
-
-export const data3 = {
-  labels2,
-  datasets: [
-    {
-      label: "유효 감정",
-      data: [578],
-      backgroundColor: "rgb(255, 99, 132)",
-    },
-    {
-      label: "그외",
-      data: [341],
-      backgroundColor: "rgb(75, 192, 192)",
-    },
-  ],
-};
-export const data2 = {
-  labels2,
-  datasets: [
-    {
-      label: "애정",
-      data: [0],
-      backgroundColor: "rgb(255, 99, 132)",
-    },
-    {
-      label: "분노",
-      data: [14],
-      backgroundColor: "rgb(75, 192, 192)",
-    },
-    {
-      label: "짜증",
-      data: [103],
-      backgroundColor: "rgb(53, 162, 235)",
-    },
-    // {
-    //   label: "기대",
-    //   data: [124],
-    //   backgroundColor: "rgb(255, 99, 132)",
-    // },
-    // {
-    //   label: "혐오",
-    //   data: [17],
-    //   backgroundColor: "rgb(75, 192, 192)",
-    // },
-    // {
-    //   label: "자신감",
-    //   data: [44],
-    //   backgroundColor: "rgb(53, 162, 235)",
-    // },
-    // {
-    //   label: "반감",
-    //   data: [98],
-    //   backgroundColor: "rgb(255, 99, 132)",
-    // },
-    // {
-    //   label: "단절",
-    //   data: [122],
-    //   backgroundColor: "rgb(75, 192, 192)",
-    // },
-    // {
-    //   label: "불안",
-    //   data: [312],
-    //   backgroundColor: "rgb(53, 162, 235)",
-    // },
-    // {
-    //   label: "고통",
-    //   data: [2],
-    //   backgroundColor: "rgb(255, 99, 132)",
-    // },
-    // {
-    //   label: "평화",
-    //   data: [1],
-    //   backgroundColor: "rgb(75, 192, 192)",
-    // },
-    // {
-    //   label: "기쁨",
-    //   data: [0],
-    //   backgroundColor: "rgb(53, 162, 235)",
-    // },
-    // {
-    //   label: "슬픔",
-    //   data: [22],
-    //   backgroundColor: "rgb(255, 99, 132)",
-    // },
-    // {
-    //   label: "감성",
-    //   data: [0],
-    //   backgroundColor: "rgb(75, 192, 192)",
-    // },
-    // {
-    //   label: "괴로움",
-    //   data: [29],
-    //   backgroundColor: "rgb(53, 162, 235)",
-    // },
-    // {
-    //   label: "놀람",
-    //   data: [31],
-    //   backgroundColor: "rgb(255, 99, 132)",
-    // },
-    // {
-    //   label: "동정",
-    //   data: [0],
-    //   backgroundColor: "rgb(75, 192, 192)",
-    // },
-    // {
-    //   label: "동경",
-    //   data: [0],
-    //   backgroundColor: "rgb(53, 162, 235)",
-    // },
-  ],
-};
-
-export const options = {
-  maintainAspectRatio: false,
-  indexAxis: "y" as const,
-  plugins: {
-    title: {
-      display: false,
-      text: "Chart.js Bar Chart - Stacked",
-    },
-  },
-  responsive: true,
-  scales: {
-    x: {
-      display: false,
-      stacked: true,
-      grace: 5,
-    },
-    y: {
-      stacked: true,
-    },
-  },
-};
